d3b61e841e2290764d2fe26856c5e30e8df2f701
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2026-01-12 16:39:05 -0400

    starting KnapsackSolver

---
+++ 7 - Dynamic Programming/KnapsackSolver.py
@@ -0,0 +1,32 @@
+class Knapsack:
+    def __init__(self, items, weights, values):
+        # TODO: adding suport to use json-like dictionary for items
+
+        self.items = items
+        self.weights = weights
+        self.values = values
+
+        self.n = len(self.items)
+    
+    def createTable(self, w):
+        return [[0] * (w + 1) for _ in range(self.n + 1)]
+    
+    def solve(self, w):
+        self.table = self.createTable(w)
+
+        for item in range(1, self.n + 1):
+            for capacity in range(1, w + 1):
+                without_current_item = self.table[item - 1][capacity]
+                with_current_item = 0
+                weight_current_item = self.weight[item - 1]
+
+                if capacity >= weight_current_item:
+                    remaining_capacity = capacity - weight_current_item
+                    with_current_item = self.value[item - 1] + self.table[item - 1][remaining_capacity]
+
+                self.table[item][capacity] = max(without_current_item, with_current_item)
+        
+        # TODO: implment next two methods
+        print('Max value: ', self.maxValue())
+        print('Selected items: ', self.selectedItems())
+    
