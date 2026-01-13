6ff8f43f189ddcaf47810f7340e8a5e1586e29f7
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2026-01-13 10:14:02 -0400

    implemented maxValue and selectedItems for KnapsakSolver

--- 7 - Dynamic Programming/KnapsackSolver.py
+++ 7 - Dynamic Programming/KnapsackSolver.py
@@ -18,15 +18,42 @@ class Knapsack:
             for capacity in range(1, w + 1):
                 without_current_item = self.table[item - 1][capacity]
                 with_current_item = 0
-                weight_current_item = self.weight[item - 1]
+                weight_current_item = self.weights[item - 1]
 
                 if capacity >= weight_current_item:
                     remaining_capacity = capacity - weight_current_item
-                    with_current_item = self.value[item - 1] + self.table[item - 1][remaining_capacity]
+                    with_current_item = self.values[item - 1] + self.table[item - 1][remaining_capacity]
 
                 self.table[item][capacity] = max(without_current_item, with_current_item)
         
         # TODO: implment next two methods
-        print('Max value: ', self.maxValue())
-        print('Selected items: ', self.selectedItems())
+        print('Max value: ', self.maxValue(w))
+        print('Selected items: ', self.selectedItems(w))
     
+    def maxValue(self, w):
+        return self.table[self.n][w]
+
+    def selectedItems(self, w):
+        selected = set()
+        item, capacity = self.n, w
+
+        while item > 0:
+            current = self.table[item][capacity]
+            previous = self.table[item - 1][capacity]
+
+            if current == previous:
+                item -= 1
+                continue
+
+            if current != previous:
+                selected.add(self.items[item - 1])
+
+                remaining = current - self.values[item - 1]
+
+                while previous != remaining and remaining > 0:
+                    capacity -= 1
+                    previous = self.table[item -1][capacity]
+
+            item -= 1
+            
+        return selected
