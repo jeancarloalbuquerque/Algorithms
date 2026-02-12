a5cec7ab87c55a08e0cb8b5d4b682bfdf4a91024
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2026-02-12 09:13:13 -0400

    update

--- Leet Code/stringGame.py
+++ Leet Code/stringGame.py
@@ -1,22 +1,27 @@
 # 3304. Find the K-th Character in String Game I
 # https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/description/?envType=problem-list-v2&envId=recursion
 
+import time
+
 class Solution(object):
-    def kthCharacter(self, k, word:str='x'):
+    def kthCharacter(self, k, word:str='a'):
         if len(word) >= k:
-            print(word)
             return word[k-1]
 
         for char in word:
             word += chr(
-                (ord(char) - ord('a') + 1) % 
-                (ord('z') - ord('a') + 1) +
-                ord('a')
+                (ord(char) - ord('a') + 1) % 26 + ord('a')
             )
 
         return self.kthCharacter(k, word)
+    
+k = 1000
 
-k = 10
+start_time = time.time()
+
 result = (Solution()).kthCharacter(k)
 
-print(result)
+time_end = time.time() - start_time
+
+print(f'k = {k} -> {result}')
+print(f'\n\n--- Run time: {time_end :.3f} s ---')
